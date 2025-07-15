316ffe2b666b2e293dd59c6eb7c8e0d920c2dc17
Lucas Lopes <66391853+lucaslopes-ti@users.noreply.github.com>
2025-07-15 08:01:22 -0300

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,18 +3,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float runSpeed;
-    private Rigidbody2D rb; // Para jogos 2D, use Rigidbody para 3D
+    private Rigidbody2D rb; 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Obtenha a refer ncia ao Rigidbody2D
-        // Se for 3D, use: rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
-    void FixedUpdate() // Use FixedUpdate para opera  es de f sica
+    void FixedUpdate() 
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        Vector2 movement = new Vector2(horizontalInput * runSpeed, rb.linearVelocity.y); // Mant m a velocidade vertical existente
+        Vector2 movement = new Vector2(horizontalInput * runSpeed, rb.linearVelocity.y);
         rb.linearVelocity = movement;
     }
 }
